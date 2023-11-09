35969cefc5648a5d78fad3383bf9ee8eed246d7d
Astrum93 <jungmin2670@gmail.com>
2023-11-09 19:30:38 +0900

    231107 local_json 수정

--- lib/common/util/local_json.dart
+++ lib/common/util/local_json.dart
@@ -31,22 +31,18 @@ class LocalJson {
   }
 }
 
-// T _tryConverting<T>(dynamic json) {
-//
-//
-//   switch (T) {
-//     case SimpleStock:
-//     default:
-//       return SimpleStock.fromJson(json) as T;
-//   }
-// }
-
 T _tryConverting<T>(dynamic json) {
-
-
   switch (T) {
     case Package:
-    default:
       return Package.fromJson(json) as T;
+    case SimpleStock:
+      return SimpleStock.fromJson(json) as T;
+    default:
+      throw Exception("Please check _tryConverting method");
   }
+  // switch (T) {
+  //   case ExampleVO:
+  //   default:
+  //     return ExampleVO.fromJson(json) as T;
+  // }
 }
