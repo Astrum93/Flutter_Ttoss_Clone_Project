9b5b860e337c8d78d8249d4a2618e3c4b8009748
Astrum93 <jungmin2670@gmail.com>
2023-11-18 02:02:11 +0900

    231118 Isolate

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,7 +1,7 @@
-import 'package:fast_app_base/common/cli_common.dart';
+import 'dart:convert';
+import 'dart:isolate';
+
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/common/widget/round_button_theme.dart';
-import 'package:fast_app_base/common/widget/w_round_button.dart';
 import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
 import 'package:fast_app_base/screen/main/s_main.dart';
@@ -10,7 +10,6 @@ import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_rive_like_button.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:live_background/live_background.dart';
 import 'package:live_background/widget/live_background_widget.dart';
 
@@ -55,11 +54,15 @@ class _HomeFragmentState extends State<HomeFragment> {
                   SizedBox(
                     height: 250,
                     width: 250,
-                    child: RiveLikeButton(isLike, onTapLike: (isLike) {
-                      setState(() {
-                        this.isLike = isLike;
-                      });
-                    }),
+                    child: RiveLikeButton(
+                      isLike,
+                      onTapLike: (bool isLike) async {
+                        await veryHeavyComputationWorkWithIsolateSpawn();
+                        setState(() {
+                          this.isLike = isLike;
+                        });
+                      },
+                    ),
                   ),
                   height10,
                   BigButton(
@@ -141,4 +144,77 @@ class _HomeFragmentState extends State<HomeFragment> {
   void openDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
   }
+}
+
+Future<void> veryHeavyComputationWorkWithIsolateSpawn() async {
+  final errorPort = ReceivePort();
+  errorPort.listen((element) {
+    debugPrint('Isolate Error!');
+    debugPrint(element);
+  });
+  final exitPort = ReceivePort();
+  exitPort.listen((message) {
+    debugPrint('Exit - Done');
+  });
+  final progressListenPort = ReceivePort();
+  progressListenPort.listen((message) {
+    debugPrint('received from isolate');
+    debugPrint(message.toString());
+  });
+  final isolate = await Isolate.spawn(
+    (port) {
+      int count = 0;
+      debugPrint('Isolate Count Start');
+      final startTime = DateTime.now();
+      for (int i = 0; i <= 800000000; i++) {
+        count += 1;
+
+        if (i % 100000000 == 0) {
+          port.send(count);
+          debugPrint(
+              "${DateTime.now().difference(startTime).inMilliseconds / 1000}sec");
+        }
+
+        // if (i % 10000000 == 0) {
+        //   throw Exception('error');
+        // }
+      }
+      debugPrint(count.toString());
+      debugPrint(
+          "${DateTime.now().difference(startTime).inMilliseconds / 1000}sec");
+    },
+    progressListenPort.sendPort,
+    onError: errorPort.sendPort,
+    onExit: exitPort.sendPort,
+  );
+
+  debugPrint('spawn done');
+  delayTimer(() {
+    // debugPrint('force kill');
+    // isolate.kill(priority: Isolate.immediate);
+
+    // debugPrint('force exit isolate');
+    // Isolate.exit(isolate.controlPort);
+  }, 500.ms);
+}
+
+///Flutter 3.7 이상
+void veryHeavyComputationWorkWithIsolateRun() async {
+  const message = '{"message": "Flutter is good"}';
+  final isolateResult = await Isolate.run<String>(() {
+    final jsonObject = json.decode(message);
+    debugPrint(jsonObject["message"]);
+    int count = 0;
+    debugPrint('Isolate Count Start');
+    final startTime = DateTime.now();
+    for (int i = 0; i <= 900000000; i++) {
+      count += 7;
+    }
+
+    debugPrint(count.toString());
+    debugPrint(
+        "${DateTime.now().difference(startTime).inMilliseconds / 1000}sec");
+    return "Run Isolate Done";
+  });
+  debugPrint(isolateResult);
 }
